66d445c9997603c387286e6863178755529c10d4
deloire <sashamax098760@gmail.com>
2025-02-01 23:40:11 +0300

    feat: add getCoordinates method which returns longitude and latitude as Map

--- src/main/java/weather/Coordinates.java
+++ src/main/java/weather/Coordinates.java
@@ -1,10 +1,15 @@
 package weather;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 
@@ -14,10 +19,11 @@ public class Coordinates {
    private float longitude;
    private final ResourceBundle bundle = ResourceBundle.getBundle("application");
    private final String GEOCODER_URL = bundle.getString("url.geocoder");
+   private final OkHttpClient client = new OkHttpClient();
+   private final ObjectMapper mapper = new ObjectMapper();
 
 
    public String getLocationInfo(String address) throws IOException {
-       OkHttpClient client = new OkHttpClient();
        Request request = new Request.Builder()
                .url(GEOCODER_URL
                + "?apikey=" + bundle.getString("api.geocoder")
@@ -28,6 +34,25 @@ public class Coordinates {
        try (Response response = client.newCall(request).execute()){
            return response.body().string();
        }
+   }
+
+   public Map<String, Float> getCoordinates(String locationInfo) throws JsonProcessingException {
+       Map<String, Float> location = new HashMap<>();
+       JsonNode node = mapper.readTree(locationInfo);
+       String coordinates = node.get("response")
+               .get("GeoObjectCollection")
+               .get("featureMember")
+               .get(0)
+               .get("GeoObject")
+               .get("Point")
+               .get("pos").asText();
+
+       setLatitude(Float.parseFloat(coordinates.split(" ")[0]));
+       setLongitude(Float.parseFloat(coordinates.split(" ")[1]));
+
+       location.put("latitude", getLatitude());
+       location.put("longitude", getLongitude());
+       return location;
    }
 
     public float getLatitude() {
--- src/main/java/weather/Main.java
+++ src/main/java/weather/Main.java
@@ -1,24 +1,12 @@
 package weather;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        ObjectMapper mapper = new ObjectMapper();
-
-
         Coordinates coordinates = new Coordinates();
-        JsonNode node = mapper.readTree(coordinates.getLocationInfo("New-York, USA"));
-        System.out.println(node.get("response")
-                .get("GeoObjectCollection")
-                .get("featureMember")
-                .get(0)
-                .get("GeoObject")
-                .get("Point")
-                .get("pos").asText());
+        String location = coordinates.getLocationInfo("Moscow, Russia");
+        System.out.println(coordinates.getCoordinates(location));
     }
 }
