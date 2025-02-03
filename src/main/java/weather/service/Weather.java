6b5863c5fb5e9e89e4c2658800c9800ee167d426
deloire <sashamax098760@gmail.com>
2025-02-03 23:48:02 +0300

    refacor: simplified getWeatherInfo method and getWeratherForecast now returns WeatherDTO

--- src/main/java/weather/service/Weather.java
+++ src/main/java/weather/service/Weather.java
@@ -1,15 +1,13 @@
-package weather;
+package weather.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import weather.api.ApiClient;
+import weather.dto.CoordinatesDTO;
+import weather.dto.WeatherDTO;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.ResourceBundle;
 
 public class Weather {
@@ -17,49 +15,34 @@ public class Weather {
     private final ResourceBundle bundle = ResourceBundle.getBundle("application");
     private final String WEATHER_URL = bundle.getString("url.weather");
     private final String WEATHER_API = bundle.getString("api.weather");
-    private final OkHttpClient client = new OkHttpClient();
-    private final ObjectMapper mapper = new ObjectMapper();
+    private final ApiClient client;
+    private final ObjectMapper mapper;
 
-    public String getWeatherInfo(Map<String, Float> coordinates) throws IOException {
+    public Weather(ApiClient client, ObjectMapper mapper) {
+        this.client = client;
+        this.mapper = mapper;
+    }
 
-        Request request = new Request.Builder()
-                .url(WEATHER_URL
+    public String getWeatherInfo(CoordinatesDTO coordinates) throws IOException {
+
+        String url = WEATHER_URL
                 + "?api_key=" + WEATHER_API
-                + "&lat=" + coordinates.get("longitude")
-                + "&lon=" + coordinates.get("latitude"))
-                .build();
+                + "&lat=" + coordinates.getLatitude()
+                + "&lon=" + coordinates.getLongitude();
 
-        try (Response response = client.newCall(request).execute()) {
-            return response.body().string();
-        }
 
+        return client.get(url);
     }
 
 
-    public Map<String, Float> getWeatherForecast(String weatherInfo) throws JsonProcessingException {
-        Map<String, Float> forecast = new HashMap<>();
-
+    public WeatherDTO getWeatherForecast(String weatherInfo) throws JsonProcessingException {
         JsonNode node = mapper.readTree(weatherInfo);
-        String currentWeather = node.get("data")
-                .get("currently")
-                .get("temperature")
-                .asText();
+        JsonNode currentlyNode = node.at("/data/currently");
 
-        String feelsLikeWeather = node.get("data")
-                .get("currently")
-                .get("apparentTemperature")
-                .asText();
-
-        String windSpeed = node.get("data")
-                .get("currently")
-                .get("windSpeed")
-                .asText();
-
-        forecast.put("currentWeather", Float.parseFloat(currentWeather));
-        forecast.put("feelsLikeWeather", Float.parseFloat(feelsLikeWeather));
-        forecast.put("windSpeed", Float.parseFloat(windSpeed));
-
-        return forecast;
+        float temperature = currentlyNode.path("temperature").floatValue();
+        float feelsLike = currentlyNode.path("apparentTemperature").floatValue();
+        float windSpeed = currentlyNode.path("windSpeed").floatValue();
+        return new WeatherDTO(temperature, feelsLike, windSpeed);
     }
 
 
