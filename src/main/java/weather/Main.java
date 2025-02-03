a1a085e0a70ce2a1a649a70f25610cf7330a49af
deloire <sashamax098760@gmail.com>
2025-02-03 23:52:43 +0300

    refactor: the code is now more readable, now you can find out the weather in several cities at once, and exit at any time

--- src/main/java/weather/Main.java
+++ src/main/java/weather/Main.java
@@ -1,16 +1,51 @@
 package weather;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import weather.api.ApiClient;
+import weather.dto.CoordinatesDTO;
+import weather.dto.WeatherDTO;
+import weather.service.Coordinates;
+import weather.service.Weather;
+import weather.util.Printer;
+
 import java.io.IOException;
-import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Weather weather = new Weather();
-        Coordinates coordinates = new Coordinates();
-        String location = coordinates.getLocationInfo("Berlin, Germany");
-        Map<String, Float> info = coordinates.getCoordinates(location);
-        String w = weather.getWeatherInfo(info);
-        Map<String, Float> forecats = weather.getWeatherForecast(w);
-        Printer.showResult(forecats);
+
+       Scanner scanner = new Scanner(System.in);
+       ApiClient client = new ApiClient();
+       ObjectMapper mapper = new ObjectMapper();
+
+
+       Coordinates coordinates = new Coordinates(client, mapper);
+       Weather weather = new Weather(client, mapper);
+
+       while (true) {
+           try {
+               System.out.print("Enter your location(example: Berlin/Germany) or 'exit': ");
+               String location = scanner.nextLine();
+               if (location.equalsIgnoreCase("exit")) {
+                   System.out.println("Bye Bye!");
+                   break;
+               }
+
+               String locationInfo = coordinates.getLocationInfo(location);
+               CoordinatesDTO coordinatesDTO = coordinates.getCoordinates(locationInfo);
+
+               String  weatherInfo = weather.getWeatherInfo(coordinatesDTO);
+               WeatherDTO forecast = weather.getWeatherForecast(weatherInfo);
+
+               Printer.showResult(forecast);
+           } catch (IOException e) {
+               System.err.println(e.getMessage());
+           } catch (IllegalArgumentException e) {
+               System.err.println(e.getMessage());
+           }
+       }
+
+       scanner.close();
+
     }
 }
